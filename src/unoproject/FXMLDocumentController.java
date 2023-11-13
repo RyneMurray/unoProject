4619f53f993c0abae3c9f3348eb0a354d6f3d977
RyneMurray <murrayryne@gmail.com>
2023-11-13 11:52:51 -0500

    Commit 2
    Filling out the display, not all elements fully implemented yet

--- src/unoproject/FXMLDocumentController.java
+++ src/unoproject/FXMLDocumentController.java
@@ -9,7 +9,14 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 
 /**
  *
@@ -17,10 +24,35 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
     
     @FXML
+    private AnchorPane Anchor;
+    @FXML
+    private ImageView start;
+    @FXML
+    private StackPane discardPile;
+    @FXML
+    private HBox computerHBox;
+    @FXML
+    private StackPane drawPile;
+    @FXML
+    private Button drawButton;
+    @FXML
+    private TextArea txtField;
+    @FXML
+    private Button red;
+    @FXML
+    private Button green;
+    @FXML
+    private Button yellow;
+    @FXML
+    private Button blue;
+    @FXML
+    private HBox controlledPlayerHBox;
+    @FXML
+    private ImageView logo;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -30,5 +62,37 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleStartButton(MouseEvent event) {
+    }
+
+    @FXML
+    private void drawClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void redClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void greenClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void yellowClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void blueClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void select(MouseEvent event) {
+    }
+
+    @FXML
+    private void mouseMoved(MouseEvent event) {
+    }
     
 }
--- src/unoproject/UnoProject.java
+++ src/unoproject/UnoProject.java
@@ -18,7 +18,7 @@ public class UnoProject extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("UnoInterface.fxml"));
         
         Scene scene = new Scene(root);
         
